8431a150059a1843b20ab55c77f55cb8ab48d42c
reba0p <reba.00p@gmail.com>
2025-03-06 22:09:16 +0300

    Update page.tsx

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NoData from "@/components/no-data";
 import { Skeleton } from "@/components/ui/skeleton";
 
 
@@ -7,9 +8,10 @@ export default function BookmarksPage() {
 
 
   return (
-    <div className="grid grid-cols-1 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-start P m-3">
-      ll
-    </div>
+    
+    <div className="items-center">
+    <NoData />
+  </div>
   );
 }
 
